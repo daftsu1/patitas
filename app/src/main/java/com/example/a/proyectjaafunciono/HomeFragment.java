055ac57f854affdc14b1bad6b9c389278232e3ef
Juan garcia <garcia.juanv97@gmail.com>
2018-06-26 03:35:20 -0400

    Agrega list view

--- app/src/main/java/com/example/a/proyectjaafunciono/HomeFragment.java
+++ app/src/main/java/com/example/a/proyectjaafunciono/HomeFragment.java
@@ -6,12 +6,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class HomeFragment extends Fragment {
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false); //define vista
+
+        String[] menuItems = {"mascota 1", "mascota 2"}; //  define lista de ejemplo
+        ListView listView = (ListView) view.findViewById(R.id.listView);
+
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
+                    getActivity(),
+                    android.R.layout.simple_list_item_1,
+                    menuItems
+                );
+
+        listView.setAdapter(listViewAdapter);
+        return view; //retorna vista con datos
     }
 }
